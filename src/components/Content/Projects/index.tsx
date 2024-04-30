b01a2286a06a96ab511b6cf048cead7930dab98d
Luis Fernando V. Leal <76924924+luisfernandovl@users.noreply.github.com>
2024-04-30 16:58:17 -0300

    Update do link e descrição do card referente ao portfólio anterior

--- src/components/Content/Projects/index.tsx
+++ src/components/Content/Projects/index.tsx
@@ -14,7 +14,7 @@ export default function Projects() {
       <ul id='project_list' className={Style.list_view}>
         <Card gif={EventTrackerGIF} image={EventTrackerJPG} title='Event Tracker' description='Sistema de agenda' tags='#React #SCSS #TypeScript' link='https://eventtracker-luisfernandovl.vercel.app/' />
         <Card gif={SorteadorGIF} image={SorteadorJPG} title='Sorteador de Amigo Secreto' description='Jogo de sorteio de amigo secreto' tags='#React #CSS #TypeScript' link='https://sorteadoramigosecreto-luisfernandovl.vercel.app/' />
-        <Card gif={PortfolioGIF} image={PortfolioJPG} title='Portfólio [Legado]' description='Site anterior a este.' tags='#HTML #CSS #JavaScript' link='https://luisfernandovl.vercel.app' />        
+        <Card gif={PortfolioGIF} image={PortfolioJPG} title='Portfólio [Legado]' description='Portfólio anterior a este.' tags='#HTML #CSS #JavaScript' link='https://luisfernandovl-old.vercel.app' />        
       </ul>
     </section>
   );
